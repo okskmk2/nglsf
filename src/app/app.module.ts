7fdac293413dcef9474c0eb922d2aa790038bc70
eunsunglee <okskmk2@naver.com>
2018-07-23 21:00:14 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JobComponent } from './job/job.component';
+import { QueueComponent } from './queue/queue.component';
+import { HostComponent } from './host/host.component';
+import { UserComponent } from './user/user.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobComponent,
+    QueueComponent,
+    HostComponent,
+    UserComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
